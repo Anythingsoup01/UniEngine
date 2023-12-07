2aea073c55f86fe05525fecb577628bd308d8148
Anythingsoup01 <williamenglish1220@gmail.com>
2023-12-06 20:09:56 -0500

    Fix

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,9 @@
 workspace "UniversalEngine"
 	architecture "x64"
+	startproject "Sandbox"
 
-	configurations{
+	configurations
+	{
 		"Debug",
 		"Release",
 		"Dist"
@@ -9,20 +11,22 @@ workspace "UniversalEngine"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
 IncludeDir = {}
 IncludeDir["GLFW"] = "UniEngine/vendor/GLFW/include"
 IncludeDir["Glad"] = "UniEngine/vendor/Glad/include"
-IncludeDir["ImGui"] = "UniEngine/vendor/ImGui"
+IncludeDir["ImGui"] = "UniEngine/vendor/imgui"
 IncludeDir["glm"] = "UniEngine/vendor/glm"
 
 include "UniEngine/vendor/GLFW"
-include "UniEngine/vendor/GLAD"
-include "UniEngine/vendor/ImGui"
+include "UniEngine/vendor/Glad"
+include "UniEngine/vendor/imgui"
 
 project "UniEngine"
 	location "UniEngine"
 	kind "SharedLib"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -30,33 +34,34 @@ project "UniEngine"
 	pchheader "uepch.h"
 	pchsource "UniEngine/src/uepch.cpp"
 
-	files {
+	files
+	{
 		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp",
 		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl"
+		"%{prj.name}/vendor/glm/glm/**.inl",
 	}
 
-	includedirs{
-		"%{prj.name}/vendor/spdlog/include",
+	includedirs
+	{
 		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
 		"%{IncludeDir.ImGui}",
 		"%{IncludeDir.glm}"
 	}
 
-	links {
+	links 
+	{ 
 		"GLFW",
 		"Glad",
 		"ImGui",
-		"opengl32.lib",
-		"dwmapi.lib"
+		"opengl32.lib"
 	}
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
 
 		defines
@@ -68,51 +73,53 @@ project "UniEngine"
 
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
 	filter "configurations:Debug"
-		defines 
-		{ 
-			"UE_DEBUG",
-			"UE_ENABLE_ASSERTS"
-		}
+		defines "UE_DEBUG"
+		runtime "Debug"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "UE_RELEASE"
+		runtime "Release"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "UE_DIST"
+		runtime "Release"
 		optimize "On"
 
 project "Sandbox"
 	location "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files {
+	files
+	{
 		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp"
 	}
 
-	includedirs{
+	includedirs
+	{
 		"UniEngine/vendor/spdlog/include",
 		"UniEngine/src",
 		"%{IncludeDir.glm}"
 	}
 
-	links {
+	links
+	{
 		"UniEngine"
 	}
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
 
 		defines
@@ -122,12 +129,15 @@ project "Sandbox"
 
 	filter "configurations:Debug"
 		defines "UE_DEBUG"
+		runtime "Debug"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "UE_RELEASE"
-		symbols "On"
+		runtime "Release"
+		optimize "On"
 
 	filter "configurations:Dist"
 		defines "UE_DIST"
-		symbols "On"
+		runtime "Release"
+		optimize "On"
